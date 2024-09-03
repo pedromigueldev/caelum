caaf93fc31aed40b3b24ad291196b0ac86121e2a
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2024-09-02 22:17:04 -0300

    mock data

--- app/api/find/route.ts
+++ app/api/find/route.ts
@@ -1,13 +1,17 @@
 import { fetchApi } from "@/utils/fetchApi";
 import { useSearchParams } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
+import { promises as fs } from 'fs';
 
 export async function GET(request: NextRequest) {
+  const file = await fs.readFile(process.cwd() + '/public/data.json', 'utf8').then((data:string) => 
+    JSON.parse(data)
+  );
 
   let longitude: string = request.nextUrl.searchParams.get("lon") || "0";
   let latitude: string = request.nextUrl.searchParams.get("lat") || "0";
   
-  let data = await fetchApi(parseFloat(latitude), parseFloat(longitude));
+  // let data = await fetchApi(parseFloat(latitude), parseFloat(longitude));
 
-  return NextResponse.json(data)
+  return NextResponse.json(file)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 'use client'
+import { Weather } from "@/utils/watherInterface";
 import { NextResponse } from "next/server";
 import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [temp, setTemp] = useState<number|null>(100);
+  const [data, setData] = useState<Weather|null>();
 
   const getWeatherData = async (lon: number, lat: number) => {
+
     let url = new URL('http://localhost:3000/api/find/')
     url.searchParams.append("lon", lon.toString());
     url.searchParams.append("lat", lat.toString());
 
     try {
       const res = await fetch(url.toString(), {method: "GET"});
-      return await res.json();
+      const data: Weather = await res.json();
+      return data;
     } catch (error) {
       console.log(error);
     }
@@ -24,8 +27,9 @@ export default function Home() {
       navigator.geolocation.getCurrentPosition(
         async (position: GeolocationPosition) => {
           const { latitude, longitude } = position.coords;
-          const data = getWeatherData(longitude, latitude);
-          console.log(await data);
+          const data = await getWeatherData(longitude, latitude);
+          setData(data);
+          console.log(data);
         },
         (error: GeolocationPositionError) => {
           console.log('Error getting location: ' + error.message);
@@ -37,10 +41,16 @@ export default function Home() {
   }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center gap-2 scale-150 font-light">
-        <h2>city</h2>
-        <h1 className="text-5xl scale-150">{temp}</h1>
-        <h3>How's the weather</h3>
+      <div className="flex flex-col items-center gap-3 scale-150 font-light">
+        <h2>{data?.sys.country} - {data?.name}</h2>
+        <h1 className="text-5xl scale-150">{Math.floor(data? data?.main.feels_like:0)+"°"}</h1>
+        <div className="flex flex-col items-center">
+          <h3>{data ? data?.weather[0].description.charAt(0).toLocaleUpperCase() + data?.weather[0].description.slice(1) : ""}</h3>
+          <div className="flex gap-2 font-thin text-xs">
+            <p>Max:{data?.main.temp_max} </p>
+            <p>Min:{data?.main.temp_min} </p>
+          </div>
+        </div>
       </div>
     </main>
   );
